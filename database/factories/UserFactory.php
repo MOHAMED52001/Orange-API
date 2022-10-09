b16ec5d3c610ca8ceb34f0398f8404cb04c093d2
Mohamed Atef <mohamedatef54200@gmail.com>
2022-10-09 19:37:52 +0200

    Add Default Admin Seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,13 +17,17 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
+        $password = bcrypt("1234");
+
+        return
+            [
+                'fname' => 'ODC',
+                'lname' => 'ODC',
+                'email' => 'ODCadmin@orange.com',
+                'national_id' => "3010541545266",
+                'phone' => "02182035453",
+                'password' => $password
+            ];
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,18 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        // ['fname', 'lname', 'national_id', 'email', 'phone', 'password'];
-
-        $password = bcrypt("1234");
-
-        \App\Models\User::factsory()->create([
-            'fname' => 'ODC',
-            'lname' => 'ODC',
-            'email' => 'ODCadmin@orange.com',
-            'national_id' => "3010541545266",
-            'phone' => "02182035453",
-            'password' => $password
-        ]);
+        \App\Models\User::factory(1)->create();
     }
 }
